8d4069aea4254b460887b93a2873753336092a9a
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-03-03 13:10:43 -0600

    added file to deal with the actual running of the overall program

---
+++ src/middleEarthMadness/MiddleEarthApp.java
@@ -0,0 +1,18 @@
+package middleEarthMadness;
+
+public class MiddleEarthApp {
+
+}
+/**
+*Here I plan to build the actual funcionatlity of the app
+*it will ask for inputs from the user to do all of the following
+*1 add new character
+*2 view all characters
+*3 update characters
+*4 delete characters
+*5 attack eachother
+*6 exit
+*
+*everything should be set up in the character manager and you just need to call each of the functions
+*
+*/
